e0a5e34cd8eb464dac473d816eb91be90f5af679
Roman Kalinichenko <romankrv@gmail.com>
2011-01-30 00:00:26 +0200

    Added 'my-make-three-windows' func

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -105,3 +105,21 @@
           "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
           "culpa qui officia deserunt mollit anim id est laborum."))
 
+
+(defun my-make-three-windows () 
+"
+|-----------+-----------|
+|           |           |
+|           |           |
+|-----------+           |
+|           |           |
+|           |           |
+|-----------+-----------|
+"
+(interactive)
+  (delete-other-windows)
+  (split-window-horizontally)
+  (split-window-vertically)	
+  )
+
+   
