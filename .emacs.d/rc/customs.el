a4672710c248865e080f362c9b222e351a841937
Roman Kalinichenko <romankrv@gmail.com>
2011-05-22 18:36:31 +0300

    customize: desktop mode

--- .emacs.d/rc/customs.el
+++ .emacs.d/rc/customs.el
@@ -4,6 +4,9 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(desktop-load-locked-desktop (quote ask))
+ '(desktop-path (quote ("." "~/.emacs.d/" "")))
+ '(desktop-save t)
  '(display-time-24hr-format t)
  '(erc-email-userid "romankrv@gmail.com")
  '(erc-log-file-coding-system (quote utf-8))
