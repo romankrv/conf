bcc848d99f8acb49e3e8f9642984c188b46c8ab4
Roman Kalinichenko <romankrv@gmail.com>
2011-06-20 00:07:37 +0300

    Added mode that allow auto-refresh FF when save html or css files in emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -125,3 +125,43 @@
                     (interactive)
                     (comint-send-string (inferior-moz-process)
                                         "BrowserReload();")))
+
+
+(defun auto-reload-firefox-on-after-save-hook ()         
+          (add-hook 'after-save-hook
+                       '(lambda ()
+                          (interactive)
+                          (comint-send-string (inferior-moz-process)
+                                              "setTimeout(BrowserReload(), \"1000\");"))
+                       'append 'local)) ; buffer-local
+
+;; Example - you may want to add hooks for your own modes.
+;; I also add this to python-mode when doing django development.
+(add-hook 'html-mode-hook 'auto-reload-firefox-on-after-save-hook)
+(add-hook 'css-mode-hook 'auto-reload-firefox-on-after-save-hook)
+
+
+
+(require 'moz)
+
+;;; Usage
+;; Run M-x moz-reload-mode to switch moz-reload on/off in the
+;; current buffer.
+;; When active, every change in the buffer triggers Firefox
+;; to reload its current page.
+
+(define-minor-mode moz-reload-mode
+  "Moz Reload Minor Mode"
+  nil " Reload" nil
+  (if moz-reload-mode
+      ;; Edit hook buffer-locally.
+      (add-hook 'post-command-hook 'moz-reload nil t)
+    (remove-hook 'post-command-hook 'moz-reload t)))
+
+(defun moz-reload ()
+  (when (buffer-modified-p)
+    (save-buffer)
+    (moz-firefox-reload)))
+
+(defun moz-firefox-reload ()
+  (comint-send-string (inferior-moz-process) "BrowserReload();"))
