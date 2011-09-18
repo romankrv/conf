45857afc1eb788fbfd0d1b61f48070b4c16beb67
Roman Kalinichenko <romankrv@gmail.com>
2011-09-19 01:53:39 +0300

    Chenge configurate for first start it simpler installer for el-get plugin see

--- .emacs.d/rc/preload-interface.el
+++ .emacs.d/rc/preload-interface.el
@@ -1,5 +1,15 @@
-;; Interface
+;; Install el-get plugin
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp))))
+
+
+;; Interface
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
