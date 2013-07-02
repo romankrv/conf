1125c2243adb35b4e896f1d598764f10e6a74a9b
Roman Kalinichenko <romankrv@gmail.com>
2013-07-02 11:57:42 +0300

    Update preload.el
    el-get -> update setup conf

--- .emacs.d/rc/preload.el
+++ .emacs.d/rc/preload.el
@@ -1,12 +1,15 @@
 ;; Install el-get plugin
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-(unless (require 'el-get nil t)
-  (url-retrieve
-   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-   (lambda (s)
-     (end-of-buffer)
-     (eval-print-last-sexp))))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+
+(el-get 'sync)
 
 
 ;; Interface
