fd05671fd9c23a5ad0f7bcf667116fdf60b41752
Roman Kalinichenko <romankrv@gmail.com>
2013-09-30 12:08:19 +0300

    Very a big refactoring

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,41 +1,129 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; GNU Emacs configuration
-;; (c) Roman Kalinichenko 2010-2013
-;; https://github.com/romankrv/conf       romankrv AT gmail.com
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                                                 ;;
+;;                      GNU Emacs configuration                    ;;
+;;                                                                 ;;
+;;             (c) R o m a n  K a l i n i c h e n k o              ;;
+;;                                                                 ;;
+;;                          (c)2010-2013                           ;;
+;;                                                                 ;;
+;;                 https://github.com/romankrv/conf                ;;
+;;                                                                 ;;
+;;            r o m a n k r v  A T  g m a i l  D O T  c o m        ;;
+;;                                                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(load-file "~/.emacs.d/rc/preload.el")
-(add-to-list 'load-path "~/.emacs.d/packages/")
 
+;; U I / U X
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (blink-cursor-mode -1))
 
-;; el-get Package Manager for Emacs like apt-get, macport, brew etc 
-(require 'el-get)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(ansi-color-for-comint-mode-on)
+(set-default 'indent-tabs-mode nil)
+(set-default 'indicate-empty-lines t)
+(set-default 'imenu-auto-rescan t)
 
-;; local sources
-(setq el-get-sources
-  '(
-;;     (:name magit
-;;       :after (global-set-key (kbd "C-x C-z") 'magit-status))
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
+  (mouse-wheel-mode t))
+
+;; Platform-specific stuff
+(when (eq system-type 'darwin)
+    ;; Work around a bug on OS X where system-name is FQDN
+    (setq system-name (car (split-string system-name "\\."))))
+
+;; E L P A
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;; E L - G E T
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+
+(setq el-get-sources 
+   '((:name color-theme-railscasts :post-init (progn (color-theme-railscasts)))
+     (:name smex
+      :post-init
+          (progn
+            (global-set-key [(meta x)] (lambda ()
+                             (interactive)
+                             (or (boundp 'smex-cache)
+                                 (smex-initialize))
+                             (global-set-key [(meta x)] 'smex)
+                             (smex)))
+            (global-set-key [(shift meta x)] (lambda ()
+                                   (interactive)
+                                   (or (boundp 'smex-cache)
+                                       (smex-initialize))
+                                   (global-set-key [(shift meta x)] 'smex-major-mode-commands)
+                                   (smex-major-mode-commands)))
+            (defadvice smex (around space-inserts-hyphen activate compile)
+              (let ((ido-cannot-complete-command
+               `(lambda ()
+                  (interactive)
+                  (if (string= " " (this-command-keys))
+                      (insert ?-)
+                    (funcall ,ido-cannot-complete-command)))))
+                ad-do-it))
+            (setq smex-save-file "~/.emacs.d/temp/smex-items")
+            (smex-initialize)))
+
+     (:name fill-column-indicator
+      :post-init
+          (progn
+            (setq-default fci-rule-column 80)
+            (setq fci-rule-color "#200200200")
+            (define-globalized-minor-mode global-fci-mode fci-mode (lambda()(fci-mode 1)))
+            (global-fci-mode 1)))
+
+     (:name magit
+      :post-init
+          (progn
+            (global-set-key (kbd "C-x m") 'magit-status)
+            (autoload 'magit-status "magit")))
 ))
 
-;; standart sources from el-get repository of recipet
-(setq my-packages
+(setq my-packages ;; el-get extra modules
       (append
-       '(el-get python-mode smex ack js2-mode color-theme 
-         highlight-parentheses browse-kill-ring auto-complete)
-       (mapcar 'el-get-source-name el-get-sources)))
+       '(ack
+         el-get
+         idomenu
+         js2-mode
+         sr-speedbar
+         django-mode
+         python-mode
+         auto-complete
+         smooth-scroll
+         ace-jump-mode
+         browse-kill-ring
+         multiple-cursors) (mapcar 'el-get-source-name el-get-sources)))
 (el-get 'sync my-packages)
 
+;; see matching pairs of parentheses and other characters
+(show-paren-mode t)
+(setq show-paren-style 'expression)
+(set-face-background 'show-paren-match-face (face-background 'default))
+    (set-face-foreground 'show-paren-match-face "#def")
+    (set-face-attribute 'show-paren-match-face nil :weight 'extra-bold)
 
-;; Tramp modxre
+;; make electric-pair-mode work on more brackets
+(electric-pair-mode 1)
+(setq electric-pair-pairs '((?\" . ?\")(?\{ . ?\})))
+
+;; Tramp mode
 (require 'tramp)
 (setq tramp-default-method "ssh")
-
-;; sr-speedbar
-(require 'sr-speedbar)
-
-;; highlight-indentation
-(require 'highlight-indentation)
 
 ;; IDO plugin
 (custom-set-variables
@@ -43,63 +131,67 @@
  '(ido-mode (quote both) nil (ido)))
 (custom-set-faces)
 
-;;special custom settings
-'(setq smex-save-file "~/.emacs.d/temp/smex-items")
-'(recentf-save-file "~/.emacs.d/temp/.recentf")
-
-;; Loading additional configuration modes
-(load-file "~/.emacs.d/rc/interfaces.el")
-(load-file "~/.emacs.d/rc/conf-smex.el")
+;; Loading extra configuration
 (load-file "~/.emacs.d/rc//keybinding.el")
-(load-file "~/.emacs.d/rc/django-mode.el")
-(load-file "~/.emacs.d/rc/flymake-modes-conf.el")
+(load-file "~/.emacs.d/rc/custom-defuns.el")
+(load-file "~/.emacs.d/rc/ibuffer.el")
 
 ;; conf for M-x customize-group
 (setq custom-file "~/.emacs.d/rc/customs.el")
 (load custom-file)
 
-;; Checkout that TEMP FOLDER is exist otherwise create it
-(if (not (file-exists-p "~/.emacs.d/temp" ))
-    (make-directory "~/.emacs.d/temp"))
-
-;; htmlize  package converts the buffer text and the associated
-;; decorations to HTML. M-x htmlize- RET
-(require 'htmlize)
-
-;; Desktop mode 
-(desktop-save-mode 1)
-
-(defun lorem ()
-  "Insert a lorem ipsum."
+;; Push mark when using ido-imenu
+(defvar push-mark-before-goto-char nil)
+(defadvice goto-char (before push-mark-first activate)
+  (when push-mark-before-goto-char
+    (push-mark)))
+(defun ido-imenu-push-mark ()
   (interactive)
-  (insert "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
-          "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim"
-          "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
-          "aliquip ex ea commodo consequat. Duis aute irure dolor in "
-          "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
-          "paratur. Excepteur sint occaecat cupidatat non proident, sunt in "
-          "culpa qui officia deserunt mollit anim id est laborum."))
+  (let ((push-mark-before-goto-char t))
+    (ido-imenu)))
 
+;; Transparently open compressed files
+(auto-compression-mode t)
 
-(defun my-make-three-windows ()
-"
-|-----------+-----------|
-|           |           |
-|           |           |
-|-----------+           |
-|           |           |
-|           |           |
-|-----------+-----------|
-"
-(interactive)
-  (delete-other-windows)
-  (split-window-horizontally)
-  (split-window-vertically)
-  )
+;; scroll string by string (keyboard)
+(setq
+  scroll-margin 0
+  scroll-conservatively 100000
+  scroll-preserve-screen-position 1)
 
-(defun winpdb()
-    "Insert string with import for dubugging in winpdb"
-    (interactive)
-    (insert "import rpdb2; rpdb2.start_embedded_debugger('1')"))
+(set-face-attribute 'default nil :height 130)
 
-(highlight-indentation-mode '1)
+;; store recent files list
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key (kbd "C-c C-r") 'recentf-open-files)
+
+;; postion of the point
+(column-number-mode ())
+(line-number-mode ())
+
+;; Not allow type "yes" if file or buffer exist
+(setq default-indicate-empty-lines t)
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq confirm-nonexistent-file-or-buffer nil)
+
+;; Start from empty page (scratch-buffer)
+(setq inhibit-splash-screen nil)
+
+;; Sets null message for scratch-buffer
+(setq initial-scratch-message nil)
+
+;; set indent-tabs-mode
+(setq-default indent-tabs-mode nil)
+;(setq tab-width 4)
+
+;; To turn on syntax highlighting, parentheses matching
+(global-font-lock-mode 1)
+(show-paren-mode 1)
+(transient-mark-mode 1)
+
+;; Save point position between sessions
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file "~/.emacs.d/temp/saved-places")
--- README.emacs
+++
@@ -1,6 +0,0 @@
-Step by step for install emacs's config
-
-Rope package:
-    1) Install from pypi: http://pypi.python.org/pypi/rope 
-        # sudo pip install rope
-        or unzip from .emacs.d/packages/rope-.x.x.x.tar.gz
