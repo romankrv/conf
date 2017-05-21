09aa4c3fad5a0c1a6ecea5973dcefec702ff0b83
Roman Kalinichenko <romankrv@gmail.com>
2017-05-22 00:21:40 +0300

    Add handler/wraper for emacs.org file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,10 @@
-;;
-;(org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))
+;; This's file is a handler/wraper for emacs.org file
+;; which has a literate style of the configuration.
 
-;;
-(load-file "~/.emacs.d/emacs.el")
+(if (file-exists-p "~/.emacs.d/emacs.el")
+    (progn
+      (load-file "~/.emacs.d/emacs.el")
+      (message "You are running from the rest of emacs.el"))
+  (progn
+    (org-babel-tangle-file "~/.emacs.d/emacs.org")
+    (org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))))
