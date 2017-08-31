ee984175287d4d622e42a8b4610c1d42053d16e1
Roman Kalinichenko <romankrv@gmail.com>
2017-08-31 23:49:47 +0300

    modify init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,10 +1,26 @@
 ;; This's file is a handler/wraper for emacs.org file
 ;; which has a literate style of the configuration.
 
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;;Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+        (package-refresh-contents)
+        (package-install 'use-package))
+
+(require 'use-package)
+(use-package auto-compile
+  :config (auto-compile-on-load-mode))
+
+
 (if (file-exists-p "~/.emacs.d/emacs.el")
     (progn
       (load-file "~/.emacs.d/emacs.el")
       (message "You are running from the rest of emacs.el"))
   (progn
-    ;(org-babel-tangle-file "~/.emacs.d/emacs.org")
-    (org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))))
+    (org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org")
+                         (message "You are running from the rest of emacs.org"))))
