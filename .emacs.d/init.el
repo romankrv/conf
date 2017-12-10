807f9ea8fabecfbfd0f3f1dbb3d85b7dbc662033
Roman Kalinichenko <romankrv@gmail.com>
2017-12-10 02:40:59 +0200

    Some improve changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,23 +1,37 @@
 ;; This's the file of the manage of emacs.org file.
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 
-
 (package-initialize)
-
-;;Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
-        (package-refresh-contents)
-        (package-install 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+(setq use-package-verbose t)
 
 
 (defconst RK/emacs-directory (concat (getenv "HOME") "/.emacs.d/"))
+(defconst RK/MAIN-LOAD-FILE (concat RK/emacs-directory "elisp/init-main.el"))
+(defconst RK/MAIN-ORG-FILE (concat RK/emacs-directory "emacs.org"))
+
+;;Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+(defun RK/emacs-tangle-file (file)
+  "Given an 'org-mode' FILE, tangle the source code."
+  (interactive "fOrg File: ")
+  (find-file file)   ;;  (expand-file-name file \"$DIR\")
+  (org-babel-tangle)
+  (kill-buffer))
+
+
 (defun RK/emacs-subdirectory (d) (expand-file-name d RK/emacs-directory))
-
-
 (let* ((subdirs '("elisp" "backups"))
        (fulldirs (mapcar (lambda (d) (RK/emacs-subdirectory d)) subdirs)))
   (dolist (dir fulldirs)
@@ -26,14 +40,12 @@
       (make-directory dir))))
 
 
-(defconst MAIN-LOAD-FILE (concat RK/emacs-directory "elisp/init-main.el"))
-(defconst MAIN-BABEL-FILE (concat RK/emacs-directory "emacs.org"))
-
-
-(if (file-exists-p MAIN-LOAD-FILE)
-    (progn
-    (load-file MAIN-LOAD-FILE)
-    (message "You are running from MAIN-LOAD-FILE"))
-  (progn
-    (org-babel-load-file MAIN-BABEL-FILE)
-    (message "You are running from MAIN-BABEL-FILE")))
+(if (file-exists-p RK/MAIN-LOAD-FILE)
+ (progn
+   (load-file RK/MAIN-LOAD-FILE)
+   (message "You are running from RK/MAIN-LOAD-FILE"))
+ (progn
+   (RK/emacs-tangle-file RK/MAIN-ORG-FILE)
+   (RK/emacs-tangle-file (concat RK/emacs-directory "emacs-org.org"))
+   (RK/emacs-tangle-file (concat RK/emacs-directory "emacs-python.org"))
+   (message "You are running from TANGLE CREATION SECTION")))
