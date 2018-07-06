3687b6dcf168bee41cd4644e05603c6f40f44734
Roman Kalinichenko <romankrv@gmail.com>
2018-07-06 18:51:15 +0300

    Refactoring and add some modes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,32 +1,28 @@
 ;; This's the file of the manage of emacs.org
-
 (require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+			 ("gnu"       . "http://elpa.gnu.org/packages/")
+			 ("melpa"     . "http://melpa.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+        (package-refresh-contents)
+        (package-install 'use-package))
+(setq use-package-verbose t)
+
 
 (defconst RK/dir (concat (getenv "HOME") "/.emacs.d/"))
 (defconst RK/MAIN-ORG-FILE (concat RK/dir "emacs.org"))
 (defconst RK/MAIN-LOAD-FILE (concat RK/dir "elisp/init-main.el"))
 
-(setq package-enable-at-startup nil)
-
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-
-(package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-;;Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(setq use-package-verbose t)
 
 (defun RK/tangle-file (file)
   "Given an 'org-mode' FILE, tangle the source code."
   (interactive "fOrg File: ")
-  (find-file file)   ;;  (expand-file-name file \"$DIR\")
+  (find-file file)
   (org-babel-tangle)
   (message "Run org-babel-tangle from --> %s" file)
   (kill-buffer))
@@ -48,9 +44,10 @@
     (RK/tangle-file (concat RK/dir "emacs-org.org"))
     (RK/tangle-file (concat RK/dir "emacs-python.org"))
     (RK/tangle-file (concat RK/dir "emacs-client.org"))
-    (RK/tangle-file (concat RK/dir "emacs-server.org"))
+    ;(RK/tangle-file (concat RK/dir "emacs-server.org"))
     (when (eq system-type 'darwin)
       (RK/tangle-file (concat RK/dir "emacs-mac.org")))
+
     (if (file-exists-p RK/MAIN-LOAD-FILE)
        (progn
          (load-file RK/MAIN-LOAD-FILE)
